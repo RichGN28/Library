e27811a241c76c984757c18696faec565d836096
SrPika <104371533+RichGN28@users.noreply.github.com>
2024-11-30 13:07:59 -0600

    Organization of files into directories, looks cleaner

---
+++ build/CMakeFiles/library.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/library.dir/src/app.cpp.o"
+  "CMakeFiles/library.dir/src/app.cpp.o.d"
+  "CMakeFiles/library.dir/src/classes.cpp.o"
+  "CMakeFiles/library.dir/src/classes.cpp.o.d"
+  "CMakeFiles/library.dir/src/helpers.cpp.o"
+  "CMakeFiles/library.dir/src/helpers.cpp.o.d"
+  "library"
+  "library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
